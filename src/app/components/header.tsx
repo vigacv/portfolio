29844b0d709688af85a734786f80de9886b5e41e
Gabriel Carmelo Villaverde <vgabrielcv@gmail.com>
2025-07-30 22:45:50 -0500

    desktop menu done

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -9,15 +9,30 @@ export default function Header() {
     <header className={styles.header}>
       <h1 className={styles.title}>&lt;GC/&gt;</h1>
 
+      <nav aria-label='Main Navigation'>
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Skills</a></li>
+          <li><a href="#">Experience</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+
       <button className={`${styles.menuButton} ${styles.button}`} onClick={() => setDrawerOpen(true)}>
         <span className="material-symbols-outlined">menu</span>
       </button>
-      <div 
+      <div
         className={`${styles.drawerOverlay} ${drawerOpen ? styles.drawerOverlayVisible : styles.drawerOverlayHidden}`}
         onClick={() => setDrawerOpen(false)}
       >
-        <div className={`${styles.drawer} ${drawerOpen ? styles.drawerOpen : styles.drawerClosed}`}
-          onClick={e => e.stopPropagation()}>
+        <div
+          className={`${styles.drawer} ${drawerOpen ? styles.drawerOpen : styles.drawerClosed}`}
+          role='dialog'
+          aria-modal="true"
+          aria-label='Mobile Menu'
+          onClick={e => e.stopPropagation()}
+        >
           <div className={styles.drawerContent}>
             <button
               className={`${styles.button} ${styles.closeButton}`}
@@ -26,8 +41,8 @@ export default function Header() {
             >
               <span className="material-symbols-outlined">close</span>
             </button>
-            <nav>
-              <ul className={styles.navList}>
+            <nav aria-label='Main Navigation'>
+              <ul>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Skills</a></li>
